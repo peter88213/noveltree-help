cf31cb9ee05ff0b59860ae139ecebb61ee2bd781
peter88213 <petert88213@gmail.com>
2024-02-08 21:32:29 +0100

    Prepare v0.1

---
+++ source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'noveltree-help'
+copyright = '2024, peter88213'
+author = 'peter88213'
+release = '0.1'
+# html_logo = '_images/nLogo64.png'
+html_title = 'noveltree help'
+html_favicon = '_images/nLogo32.ico'
+'''
+html_theme_options = {
+    "logo": {
+        "link": "https://peter88213.github.io/noveltree/",
+    }
+}
+'''
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,53 @@
+.. figure:: _images/nLogo32.png
+   :alt: Logo
+
+=====================
+noveltree Online help
+=====================
+
+
+This page refers to the latest `noveltree <https://peter88213.github.io/noveltree/>`_ release. You can open it with
+**Help > Online help**.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: General
+   
+   desktop
+   basic_concepts
+   plotting
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Command reference
+
+   file_menu
+   view_menu
+   part_menu
+   chapter_menu
+   section_menu
+   characters_menu
+   locations_menu
+   items_menu
+   plot_menu
+   project_notes_menu
+   export_menu
+   import_menu
+   tools_menu
+   tree_context_menu
+   toolbar
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Properties
+   
+   project_view
+
+
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`search`
